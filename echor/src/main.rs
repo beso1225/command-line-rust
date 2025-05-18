dd1f5294e3604a611c58750389295e583c36eb3b
beso1225 <yutaroformisc@gmail.com>
2025-05-18 13:51:38 +0000

    update gitignore and rewrite with clap v4

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,38 +1,22 @@
-use clap::{Command, Arg};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+/// Rust version of echo
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
 
 fn main() {
-    // // println!(std::env::args()); // Does not work
-    // // println!("{}", std::env::args()); // Also does not work because Args is not Display
-    // println!("{:?}", std::env::args()); // This works because Args implements Debug
-
-    let matches = Command::new("echor")
-        .version("0.1.0")
-        .author("Ken Younens-Clark <kyclark@gmail.com>")
-        .about("Rust echo")
-        .arg(
-            Arg::new("text")
-                .value_name("TEXT")
-                .help("Input text")
-                .required(true)
-                .num_args(1..),
-        )
-        .arg(
-            Arg::new("omit_newline")
-                .short('n')
-                .help("Do not print newline")
-                .num_args(0),
-        )
-        .get_matches();
-
-    println!("{:#?}", matches);
-    let text= matches.get_many::<String>("text").unwrap().cloned().collect::<Vec<String>>();
-    let omit_newline = matches.contains_id("omit_newline");
-
-    // let mut ending = "\n";
-    // if omit_newline {
-    //     ending = "";
-    // }
-    // let ending = if omit_newline { "" } else { "\n" };
-    // print!("{}{}", text.join(" "), ending);
-    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
+    let args = Args::parse();
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else { "\n" }
+    );
 }
