ccbe0a77456e9e7b86fe6bf8d80f1bec59402dd4
beso1225 <yutaroformisc@gmail.com>
2025-05-20 14:49:19 +0000

    having written all of the code of v2

--- catr_v2/src/main.rs
+++ catr_v2/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if let Err(e) = catr_v2::run() {
+    if let Err(e) = catr_v2::get_args().and_then(catr_v2::run) {
         eprintln!("{}", e);
         std::process::exit(1);
     }
